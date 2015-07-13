c93c1d4a2635338a8798dfc794904defb2abdafc
Jaap <jaapkoelman@hotmail.com>
2015-07-13 23:35:48 +0200

    jltable - changed function getAssetParentId

--- admin/tables/jltable.php
+++ admin/tables/jltable.php
@@ -139,7 +139,7 @@ class JLTable extends JTable {
 	 * We provide our global ACL as parent
 	 * @see JTable::_getAssetParentId()
 	 */
-	protected function _getAssetParentId($table = null, $id = null)
+	protected function _getAssetParentId(JTable $table = null, $id = null)
 	{
 		$asset = JTable::getInstance('Asset');
 		$asset->loadByName('com_joomleague');
