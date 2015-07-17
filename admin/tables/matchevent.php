506152f0a1587af080a4a6d7b5d87ea35d88a1db
Jaap <jaapkoelman@hotmail.com>
2015-07-17 23:36:24 +0200

    Matchevent-table: some cleanup

--- admin/tables/matchevent.php
+++ admin/tables/matchevent.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright	Copyright (C) 2006-2014 joomleague.at. All rights reserved.
+ * @copyright	Copyright (C) 2006-2015 joomleague.at. All rights reserved.
  * @license		GNU/GPL, see LICENSE.php
  * Joomla! is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -8,18 +8,15 @@
  * other free or open source software licenses.
  * See COPYRIGHT.php for copyright notices and details.
  */
-
-// Check to ensure this file is included in Joomla!
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die;
 
 // Include library dependencies
 jimport( 'joomla.filter.input' );
 
 /**
-* Event Table class
+* MatchEvent Table class
 *
 * @package		Joomleague
-* @since 0.1
 */
 class TableMatchEvent extends JLTable
 {
@@ -27,12 +24,9 @@ class TableMatchEvent extends JLTable
 	 * Constructor
 	 *
 	 * @param object Database connector object
-	 * @since 1.0
 	 */
 	public function __construct( & $db )
 	{
 		parent::__construct( '#__joomleague_match_event', 'id', $db );
 	}
-
 }
-?>
