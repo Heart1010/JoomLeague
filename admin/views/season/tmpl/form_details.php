27c862b735fb1ce438c007db34b9155b1f9ea6f2
Jaap <jaapkoelman@hotmail.com>
2015-07-26 14:57:53 +0200

    season view form details - table to div

--- admin/views/season/tmpl/form_details.php
+++ admin/views/season/tmpl/form_details.php
@@ -10,14 +10,10 @@ defined('_JEXEC') or die;
 ?>
 <fieldset class="form-horizontal">
 	<legend><?php echo JText::_('COM_JOOMLEAGUE_ADMIN_SEASON_LEGEND'); ?></legend>
-	<table class="table table-striped">
-		<tbody>
 			<?php foreach ($this->form->getFieldset('details') as $field): ?>
-			<tr>
-				<td class="key"><?php echo $field->label; ?></td>
-				<td><?php echo $field->input; ?></td>
-			</tr>					
+			<div class="control-group">		
+				<div class="control-label"><?php echo $field->label; ?></div>
+				<div class="controls"><?php echo $field->input; ?></div>
+			</div>					
 			<?php endforeach; ?>
-		</tbody>
-	</table>
 </fieldset>
